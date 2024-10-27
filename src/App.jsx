2489d4805bea6c4350a42fe710550eb11a83bb93
Diwa Tomy <diwatomy04@gmail.com>
2024-10-27 21:26:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Weather from "./components/Weather"
+function App() {
+ 
+
+  return (
+    <>
+    <div className="app">
+    <Weather/>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
